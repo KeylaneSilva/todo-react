92fa58468e4e5e2f4e131232f485f45c486f5097
Keylane <keylanessilva07@gmail.com>
2021-10-30 10:00:33 -0300

    lista de itens

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
 import * as S from './style'
+import { Item } from '../../types/Item'
 
-export const ListItem = () => {
+type Props = {
+    item: Item; 
+}
+
+export const ListItem = ({item}: Props) => {
+    
+    const [isCheck, setIsCheck] = useState(item.done)
+    
     return(
-        <div>Ola Mundo</div>
+        <S.Container done={isCheck}>
+            <input 
+                type="checkbox" 
+                checked={isCheck}
+                onChange={e => setIsCheck(e.target.checked)}
+                />
+            <label>{item.name}</label>
+        </S.Container>
     )
 }
