109fd7c81802d1886a0097871b0d5b9cd53467c6
Keylane <keylanessilva07@gmail.com>
2021-10-30 03:27:21 -0300

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
 import * as C from './App.styles';
-
 import './App.css';
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem'
 
 function App() {
+
+  const [list , setList] = useState<Item[]>([
+    {id: 1, name: 'Acordar 6 da manhã', done: false},
+    {id: 2, name: 'Estudar react 8 AM', done: false}
+
+  ])
+
   return (
     <div>
       <C.Container>
         <C.Area>
           <C.Header>Lista de tarefas</C.Header>
+        
+          {/* input da lista */}
+
+          {/* listar minha lista de tarefas */}
+          {list.map((item, index) => (
+            <ListItem />
+          ))}
+
+
         </C.Area>
       </C.Container>
     </div>
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style'
+
+export const ListItem = () => {
+    return(
+        <div>Ola Mundo</div>
+    )
+}
