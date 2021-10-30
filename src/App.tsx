91812d25fa868518560d8778fefa2aa73404d481
Keylane <keylanessilva07@gmail.com>
2021-10-30 09:59:49 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import * as C from './App.styles';
 import './App.css';
 import { Item } from './types/Item'
 import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
 function App() {
 
   const [list , setList] = useState<Item[]>([
-    {id: 1, name: 'Acordar 6 da manhã', done: false},
+    {id: 1, name: 'Acordar 6 da manhã', done: true},
     {id: 2, name: 'Estudar react 8 AM', done: false}
 
   ])
@@ -19,12 +20,12 @@ function App() {
           <C.Header>Lista de tarefas</C.Header>
         
           {/* input da lista */}
+          <AddArea />
 
           {/* listar minha lista de tarefas */}
           {list.map((item, index) => (
-            <ListItem />
+            <ListItem key={index} item={item}/>
           ))}
-
 
         </C.Area>
       </C.Container>
