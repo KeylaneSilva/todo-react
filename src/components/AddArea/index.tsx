7d4f73af43200a27ddf56e04236a7e6641d0c6ca
Keylane <keylanessilva07@gmail.com>
2021-11-01 09:41:43 -0300

    finalizando area do input

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,12 +1,32 @@
+import { useState, KeyboardEvent } from 'react'
 import * as S from './styles'
 
-export const AddArea = () => {
+type Props = {
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({ onEnter }: Props) => {
+
+    const [inputText, setInputText] = useState('')
+
+    // captura teclas do teclado
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code == 'Enter' && inputText != ''){
+            onEnter(inputText)
+            setInputText('')
+        }
+    }
+
     return(
         <S.Contairner>
             <div className="image">➕</div>
             <input 
                 type="text"
-                placeholder = "Adicione sua tarefa"/>
+                placeholder = "Adicione sua tarefa"
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+                />
         </S.Contairner>
     )
 }
